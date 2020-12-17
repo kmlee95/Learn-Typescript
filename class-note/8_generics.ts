0bee8bd79fbfdcc97189f81fbfd032a4aedaaa3e
leekyungmin <kmlee95@naver.com>
2020-12-17 13:03:19 +0900

    fix :generic interface

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -50,3 +50,29 @@ function LogText1<T>(text: T):T{
 LogText1<string>('a');
 LogText1<number>(10);
 LogText1<boolean>(true);
+
+
+//인터페이스에서의 제네릭
+interface Dropdown<T>{
+    value: T;
+    selected:boolean;
+}
+
+const obj: Dropdown<string> = {value:'abc', selected:false};
+
+//제네릭의 타입 제한
+// -> T가 아니라 배열로 넘긴다
+// function logTextLength<T>(text : T[]): T[]{
+//     console.log(text.length);
+//     return text;
+// }
+// logTextLength<string>(['hi']);
+
+//제네릭의 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType{
+    length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T{
+    text.length;
+    return text;
+}
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,24 +1,31 @@
-interface Email{
-  value: string;
+interface DropdownItem<T>{
+  value: T;
   selected: boolean;
 }
-const emails: Email[] = [
+
+// interface Email{
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber{
-  value: number;
-  selected: boolean;
-}
-const numberOfProducts: ProductNumber[] = [
+// interface ProductNumber{
+//   value: number;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem<S>(item: DropdownItem<S>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -28,7 +35,7 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
